8ec1c3ecd124616983a7796bda6376e253598987
Anthoni Marie <contact@anthoni-marie.fr>
2022-03-12 22:13:41 +0100

    [FIX] - Changed default theme mode to dark nstead of light (my eyes...)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import * as React from "react"
 import {
-  ChakraProvider,
-  theme,
+    ChakraProvider, extendTheme,
+    theme,
 } from "@chakra-ui/react"
 import {Router} from "./routes/Router";
 
+const config = {
+    useSystemColorMode: false,
+    initialColorMode: "dark",
+}
+const customTheme = extendTheme({ config })
+
 export const App = () => (
-  <ChakraProvider theme={theme}>
+  <ChakraProvider theme={customTheme}>
     <Router/>
   </ChakraProvider>
 )
