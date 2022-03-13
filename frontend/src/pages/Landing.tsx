f9cff5b7a2bad21f7aebfc310ad38bc74a7ef2a7
Anthoni Marie <contact@anthoni-marie.fr>
2022-03-13 16:47:48 +0100

    [-] - Finally removing particles

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -20,8 +20,6 @@ import { useNavigate } from "react-router-dom";
 import Granim from "granim";
 import {useEffect} from "react";
 
-import Particles from "react-tsparticles";
-
 export default function Landing() {
     const navigate = useNavigate();
     useEffect(() => {
@@ -58,65 +56,6 @@ export default function Landing() {
                     zIndex:'-1',
                     width: '100%',
                     height: '100%'}}
-                />
-                <Particles
-                    id="tsparticles"
-                    options={{
-                        fpsLimit: 30,
-                        interactivity: {
-                            events: {
-                                onClick: { enable: true, mode: "push" },
-                                onHover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                    parallax: { enable: false, force: 60, smooth: 10 }
-                                },
-                                resize: true
-                            },
-                            modes: {
-                                push: { quantity: 4 },
-                                repulse: { distance: 200, duration: 0.4 }
-                            }
-                        },
-                        particles: {
-                            color: { value: "#ffffff" },
-                            move: {
-                                direction: "none",
-                                enable: true,
-                                outModes: "out",
-                                random: false,
-                                speed: 1,
-                                straight: false
-                            },
-                            number: {
-                                density: {
-                                    enable: true,
-                                    area: 200
-                                },
-                                value: 20
-                            },
-                            opacity: {
-                                animation: {
-                                    enable: true,
-                                    speed: 0.05,
-                                    sync: true,
-                                    startValue: "max",
-                                    count: 1,
-                                    destroy: "min"
-                                },
-                                value: {
-                                    min: 0,
-                                    max: 0.5
-                                }
-                            },
-                            shape: {
-                                type: "circle"
-                            },
-                            size: {
-                                value: { min: 1, max: 5 }
-                            }
-                        }
-                }}
                 />
                 <Center><SimpleLogo h="15vmin" pointerEvents="none" /></Center>
                 <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
